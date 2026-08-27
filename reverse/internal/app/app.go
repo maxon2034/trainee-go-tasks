03579aacf10d69fb4e60767e118d7e0332c31cb9
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-08-27 08:31:30 +0300

    refactor: fix project layout

---
+++ reverse/internal/app/app.go
@@ -0,0 +1,14 @@
+package app
+
+import (
+	"fmt"
+
+	"github.com/maxon2034/trainee-go-tasks/reverse/internal/reverse"
+)
+
+func Run() {
+	s := "уауау ваики"
+	fmt.Println("before: ", s)
+	fmt.Println("no lib: ", reverse.Words(s))
+	fmt.Println("with lib: ", reverse.WordsLib(s))
+}

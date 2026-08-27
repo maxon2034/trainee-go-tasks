a8e18071f0de696eebc87ebc7821711cb73c1b39
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-08-27 08:09:43 +0300

    fix: fix lowering rune case

--- internal/palindrome/palindrome.go
+++ internal/palindrome/palindrome.go
@@ -1,16 +1,14 @@
 package palindrome
 
 import (
-	"strings"
 	"unicode"
 )
 
 func Is(s string) bool {
-	s = strings.ToLower(s)
 	runes := make([]rune, 0, len(s))
 	for _, r := range s {
 		if unicode.IsLetter(r) || unicode.IsDigit(r) {
-			runes = append(runes, r)
+			runes = append(runes, unicode.ToLower(r))
 		}
 	}
 	left, right := 0, len(runes)-1
